a8baf7efd48979acd2fcdf3a00c199469d5ec949
catchjamil <catchjamil@gmail.com>
2013-09-14 07:13:19 -0700

    new class

---
+++ SWIFTDesk/src/com/sd/training/struts2/imran/CreateObject.java
@@ -0,0 +1,13 @@
+package com.sd.training.struts2.imran;
+
+public class CreateObject {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
