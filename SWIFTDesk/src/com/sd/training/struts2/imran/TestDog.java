0cea5631af29c4b6fdd0bcd6f41f7ab05fe613c8
Zeayul <zeayul.afset.cse@gmail.com>
2013-09-17 06:50:04 -0700

    i have done it

---
+++ SWIFTDesk/src/com/sd/training/struts2/imran/TestDog.java
@@ -0,0 +1,28 @@
+package com.sd.training.struts2.imran;
+
+class Animal {
+
+		   public void move(){
+		      System.out.println("Animals can move");
+		   }
+}
+		class Dog extends Animal{
+
+		   public void move(){
+		      System.out.println("Dogs can walk and run");
+		   }
+		}
+
+	     class TestDog{
+
+		   public static void main(String args[]){
+		      Animal a = new Animal();
+		      Dog b = new Dog(); 
+
+		      a.move();
+
+		      b.move();
+		   }
+		}
+
+
