30f1bb084c86cf36894c3e7c7417685a7cc68173
Md Imran <mi3081557@gmail.com>
2013-09-23 12:29:39 -0700

    kn,k

--- SWIFTDesk/src/com/sd/training/struts2/zeayul/InheritDemo.java
+++ SWIFTDesk/src/com/sd/training/struts2/zeayul/InheritDemo.java
@@ -1,17 +1,17 @@
 package com.sd.training.struts2.zeayul;
-class A{
+class Aq{
 	void method(){
 		System.out.println("nmdmn");
 	}
 }
-class B extends A{
+class Bq extends Aq{
 	void method(){
 		System.out.println("nmdmn");
 	}
 }
 public class InheritDemo {
 	public static void main(String[] args){
-		B obj=new B();
-		obj.method();
+	//	B obj=new B();
+	//	obj.method();
 	}
 }
