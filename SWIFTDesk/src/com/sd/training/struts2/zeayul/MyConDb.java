7b4f6a9a2fb12296c6faadc585f38161fcf16348
Zeayul <zeayul.afset.cse@gmail.com>
2013-09-27 10:48:28 -0700

    done

---
+++ SWIFTDesk/src/com/sd/training/struts2/zeayul/MyConDb.java
@@ -0,0 +1,15 @@
+package com.sd.training.struts2.zeayul;
+
+import java.sql.*;
+public class MyConDb {
+	
+	static Connection conJdbc() throws SQLException{
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:9999/mydatabase","myuser","123456");
+		return con;
+	}
+	static void closeJdbc(Connection con) throws SQLException{
+		con.close();
+	}
+	
+
+}
