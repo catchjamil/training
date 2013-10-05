7c0900c50a7d031d9741b9499c90b8ef11c66f78
catchjamil <catchjamil@gmail.com>
2013-10-05 05:16:54 -0700

    Hibernate connection

--- SWIFTDesk/src/com/sd/training/struts2/serviceImpl/LoginServiceImpl.java
+++ SWIFTDesk/src/com/sd/training/struts2/serviceImpl/LoginServiceImpl.java
@@ -1,22 +1,45 @@
 package com.sd.training.struts2.serviceImpl;
 
+import org.apache.struts2.ServletActionContext;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.sd.training.struts2.bean.Menu;
 import com.sd.training.struts2.bean.User;
 import com.sd.training.struts2.dao.LoginDao;
 import com.sd.training.struts2.daoimpl.LoginDaoImpl;
+import com.sd.training.struts2.listener.HibernateListener;
 import com.sd.training.struts2.service.LoginService;
+import com.sd.training.struts2.util.ApplicationContext;
+import com.sd.training.struts2.util.HibernateUtil;
 
-public class LoginServiceImpl implements LoginService{
+public class LoginServiceImpl implements LoginService {
 
 	@Override
 	public boolean authenticateUser(User user) {
 		LoginDao loginDao = new LoginDaoImpl();
 		User userByName = loginDao.getUserByName(user.getUname());
-		if(userByName != null && user.getPassword().equals(userByName.getPassword()) )
-		{
+
+		try {
+			listCustomer();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (userByName != null
+				&& user.getPassword().equals(userByName.getPassword())) {
 			return true;
-		}else 
+		} else
 			return false;
 	}
+
+	public String listCustomer() throws Exception {
+
+		Session session = HibernateUtil.openSession();
+		Menu menu = (Menu) session.createQuery("from Menu").list();
+		session.close();
+		return "Sucess";
+
+	}
 }
-
-
---
+++ SWIFTDesk/src/com/sd/training/struts2/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.sd.training.struts2.serviceImpl;
+
+import org.hibernate.Session;
+
+import com.sd.training.struts2.bean.Menu;
+import com.sd.training.struts2.bean.User;
+import com.sd.training.struts2.dao.UserDao;
+import com.sd.training.struts2.daoimpl.UserDaoImpl;
+import com.sd.training.struts2.service.UserService;
+import com.sd.training.struts2.util.HibernateUtil;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public User save(User user) {
+		UserDao userDao = new UserDaoImpl();
+		user = userDao.save(user);
+		return user;
+	}
+	
+	public String listCustomer() throws Exception {
+
+		Session session = HibernateUtil.openSession();
+		Menu menu = (Menu) session.createQuery("from Menu").list();
+		session.close();
+		return "Sucess";
+
+	}
+}
