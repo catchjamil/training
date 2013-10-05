c8a8632cff2573085ce1e1a5cb43eedbcf8883eb
catchjamil <catchjamil@gmail.com>
2013-10-05 05:12:18 -0700

    Hibernate connection

---
+++ SWIFTDesk/src/com/sd/training/struts2/daoimpl/UserDaoImpl.java
@@ -0,0 +1,30 @@
+package com.sd.training.struts2.daoimpl;
+
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.sd.training.struts2.bean.User;
+import com.sd.training.struts2.dao.UserDao;
+import com.sd.training.struts2.util.HibernateUtil;
+
+public class UserDaoImpl implements UserDao {
+
+
+	@Override
+	public User save(User uname) {
+		try{
+		Session session = HibernateUtil.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(uname);
+		transaction.commit();
+		session.close();
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return uname;
+	}
+	
+}
