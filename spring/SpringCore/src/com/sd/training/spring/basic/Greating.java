7a2fd7fc7a7597b77bcdbb194fc542927e502b28
catchjamil <catchjamil@gmail.com>
2014-03-13 09:54:36 +0530

    Vanilla

---
+++ spring/SpringCore/src/com/sd/training/spring/basic/Greating.java
@@ -0,0 +1,16 @@
+package com.sd.training.spring.basic;
+
+public class Greating {
+
+	private String sayHello;
+
+	public String getSayHello() {
+		sayHello = "Hello World...";
+		return sayHello;
+	}
+
+	public void setSayHello(String sayHello) {
+		this.sayHello = sayHello;
+	}
+	
+}
