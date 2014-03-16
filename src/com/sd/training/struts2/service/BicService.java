0f0f4b2400bc72841e02f3b35c442f476ea23187
catchjamil <catchjamil@gmail.com>
2014-03-16 18:07:43 +0530

    delete

--- src/com/sd/training/struts2/service/BicService.java
+++
@@ -1,8 +0,0 @@
-package com.sd.training.struts2.service;
-
-import com.sd.training.struts2.bean.Bic;
-
-public interface BicService {
-	public Bic save(Bic bic)throws RuntimeException;
-
-}
--- src/com/sd/training/struts2/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.sd.training.struts2.service;
-
-import java.util.List;
-
-import com.sd.training.struts2.bean.Role;
-
-public interface RoleService {
-public List<Role > getRoles();
-}
