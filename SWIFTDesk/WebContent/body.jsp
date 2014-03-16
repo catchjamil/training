a43994714e394aa1ee6604ebda775d3baa748f36
catchjamil <catchjamil@gmail.com>
2014-03-16 17:37:31 +0530

    Vanilla

--- SWIFTDesk/WebContent/body.jsp
+++
@@ -1,7 +0,0 @@
-<%@ taglib prefix="s" uri="/struts-tags" %>
-<style>
-<!--
-h2 {background-color:'yellow';}
--->
-</style>
-<h2>This Is welcome Body</h2>
--- SWIFTDesk/src/com/sd/training/struts2/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.sd.training.struts2.service;
-
-import com.sd.training.struts2.bean.User;
-
-public interface LoginService {
-
-	public boolean authenticateUser(User user);
-}
