d15a5afe9c982fd7297a21a06c665447970c6bd6
Md Imran <mi3081557@gmail.com>
2014-03-16 12:01:14 +0530

    table name change

--- SWIFTDesk/src/com/sd/training/struts2/bean/Transfer.java
+++ SWIFTDesk/src/com/sd/training/struts2/bean/Transfer.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="Contact")
+@Table(name="Transfer_info")
 public class Transfer implements Serializable {
 
 	private static final long serialVersionUID = 1L;
