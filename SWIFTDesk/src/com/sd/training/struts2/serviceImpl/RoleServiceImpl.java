65f1f881aa6bd22fcba9e95f40367f823f341fc9
ISHTIYAQUEALAM <ialam1019@gmail.com>
2014-03-17 19:18:26 +0530

    Role service some method is added

--- SWIFTDesk/src/com/sd/training/struts2/serviceImpl/RoleServiceImpl.java
+++ SWIFTDesk/src/com/sd/training/struts2/serviceImpl/RoleServiceImpl.java
@@ -3,17 +3,34 @@ package com.sd.training.struts2.serviceImpl;
 import java.util.List;
 
 import com.sd.training.struts2.bean.Role;
+import com.sd.training.struts2.dao.RoleDao;
 import com.sd.training.struts2.daoimpl.RoleDaoImpl;
 import com.sd.training.struts2.service.RoleService;
 
-public class RoleServiceImpl implements RoleService {
+public class RoleServiceImpl implements RoleService{
+
+	@Override
+	public Role save(Role role) {
+		RoleDao roleDao = new RoleDaoImpl();
+		//Role role=null;
+		
+			role=roleDao.setData(role);
+	
+		return role;
+	}
+
+	@Override
+	public List<Role> getRoleList() {
+		RoleDao roleDao = new RoleDaoImpl();
+	   List<Role> list=roleDao.getRoleList();
+		return list;
+	}
 
 	@Override
 	public List<Role> getRoles() {
-		// TODO Auto-generated method stub
-		
-		RoleDaoImpl roleDaoImpl = new RoleDaoImpl();
-		return roleDaoImpl.getRoles();
+		RoleDao roleDao = new RoleDaoImpl();
+		   List<Role> list=roleDao.getRoles();
+			return list;
 	}
-
+	
 }
